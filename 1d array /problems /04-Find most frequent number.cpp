fa4a6d79fd5294c293f7a046fc39e4164ee6d4f7
Ahmed Ezzat <65311452+ahmedezzat1@users.noreply.github.com>
2024-03-13 16:21:44 +0200

    Update 04-Find most frequent number.cpp

--- 1d array /problems /04-Find most frequent number.cpp
+++ 1d array /problems /04-Find most frequent number.cpp
@@ -1,1 +1,40 @@
-/
+/*
+ *
+ Practice: Find most frequent number
+● Read an Integer N, then read N <= 200 integers. Find the value that repeated
+the most number of times.
+○ Each integer is 0 <= integer <= 150
+● Example for array: 1 2 1 3 1 5 5
+○ 1 repeated 3 times: the largest
+○ 2 repeated 1 time
+○ 5 repeated 2 times
+ */
+
+/**************** Solution start from here ***********************/
+int main() {
+    file();
+    fast
+    int tc = 1;
+    //cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        int frq[200]{};
+        for (int i = 0; i < n; ++i) {
+            frq[a[i]]++;
+        }
+        int val = 0, mostFrq = 0;
+
+        for (int i = 0; i < 200; ++i) {
+            if (frq[i] > mostFrq)
+                val = i, mostFrq = frq[i];
+        }
+
+        cout << val << " Repeted " << mostFrq << endl;
+    }
+}
